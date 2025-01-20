d78cf0973c48e2346c85c51df493efda6125aaee
YNDmitry <mailmetelev@gmail.com>
2025-01-21 01:50:03 +0500

    Auto publish

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { selectionAllButton_func } from '$utils/selection-all-button';
 import { coloredSlider_func } from '$utils/sliders-colors';
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  console.log('2');
+  console.log('3');
   cityDetector_func();
   formSelectors_func();
   expParams_func();
